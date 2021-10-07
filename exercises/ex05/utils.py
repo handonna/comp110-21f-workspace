cf3f336e1b84e7e9a364b6968d5b6844271db9f5
Donna Han <hdonna@primary.email>
2021-10-06 21:26:11 -0400

    ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,50 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730400224"
 
 # Define your functions below
+
+
+def only_evens(list1: list[int]) -> list:
+    """Return list containing only even elements of the input list."""
+    i: int = 0
+    list2: list[int] = []
+    while i < len(list1):
+        if list1[i] % 2 == 0:
+            list2.append(list1[i])
+        i += 1
+    return list2
+
+
+def sub(list1: list[int], start: int, stop: int) -> list:
+    """Given a list of ints, start index, and end index, generate list between indexes. """
+    list2: list[int] = []
+    i: int = 0
+    if i == len(list1): 
+        return list2
+    elif start > len(list1): 
+        return list2
+    elif stop <= i:
+        return list2
+    else: 
+        while  start < stop:
+            start < len(list1)
+            list2.append(list1[start])
+            start += 1
+        return list2
+
+
+def concat(list1: list[int], list2: list[int]) -> list:
+    """Given two lists of ints, makes new list with elements of first list followed by elements of second list."""
+    listnew: list[int] = []
+    k: int = 0
+    p: int = 0
+    while k < len(list1):
+        if k < len(list1):
+            listnew.append(list1[k]) 
+        k += 1
+    while p < len(list2):
+        if p < len(list2):
+            listnew.append(list2[p])
+        p += 1
+    return listnew
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,54 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730400224"
+
+def test_only_evens_no_evens() -> None:
+    """Edge Case, No evens are in the list and should result in empty list."""
+    list1: list[int] = [1,3,5,7]
+    assert only_evens(list1) == []
+
+def test_only_evens_some_evens() -> None:
+    """Use Case, Some evens are in the list."""
+    list1: list[int] = [10,11,12,13,14,15]
+    assert only_evens(list1) == [10,12,14]
+
+def test_only_evens_some_evens_again() -> None:
+    """Use Case, Some evens are in the list."""
+    list1: list[int] = [60,61,62,63,64,65,66]
+    assert only_evens(list1) == [60,62,64,66]
+
+def test_sub_length_of_list_is_zero() -> None:
+    """Edge Case, given list has zero elements, should result in empty list."""
+    list1: list[int] = []
+    assert sub(list1,2,4) == []
+
+def test_sub_random_numbers() -> None:
+    """Use Case, subset of list using random numbers."""
+    list1: list[int] = [0,1,2,3,4,5]
+    assert sub(list1,1,3) == [1,2]
+
+def test_sub_random_numbers_again() -> None:
+    """Use Case, subset of list using random numbers."""
+    list1: list[int] = [10,20,30,40,50]
+    assert sub(list1,1,4) == [20,30,40]
+
+def test_concat_one_list_is_empty() -> None:
+    """Edge Case, one of the lists are empty"""
+    list1: list[int] = [1,2,3,4]
+    list2: list[int] = []
+    assert concat(list1,list2) == [1,2,3,4]
+
+def test_concat_random_numbers() -> None:
+    """Use Case, concatenated list with random numbers"""
+    list1: list[int] = [20,21,22,23]
+    list2: list[int] = [24,25,26,27]
+    assert concat(list1,list2) == [20,21,22,23,24,25,26,27]
+
+def test_concat_random_numbers_again() -> None:
+    """Edge Case, concatenated list with random numbers"""
+    list1: list[int] = [10,20,30,40,50]
+    list2: list[int] = [60,70,80,90,100]
+    assert concat(list1,list2) == [10,20,30,40,50,60,70,80,90,100]
